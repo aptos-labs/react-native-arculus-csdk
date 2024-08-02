3e9ca1510e60661c003a19bec406e5c3e0dd23f5
maykonmichel <maykonmichelpalma@gmail.com>
2024-08-02 08:33:00 -0700

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  ReactNativeArculusCsdkExample
+//
+
+import Foundation
---
+++ ios/ReactNativeArculusCsdk.swift
@@ -0,0 +1,8 @@
+@objc(ReactNativeArculusCsdk)
+class ReactNativeArculusCsdk: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
